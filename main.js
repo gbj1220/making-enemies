d53378c17f13d2354c1f480d3edcb5902f294ede
Gregory.Johnson <3531op@gmail.com>
2020-10-18 15:58:56 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -1,12 +1,76 @@
 // Your code here.
+const makeBoss = function (obj) {
+  const newObj = {
+    name: obj.name,
+    attackDamage: obj.attackDamage,
+    boss: true,
+    level: obj.level,
+    alive: obj.alive,
+    hitPoints: obj.hitPoints
+  }
+  
+  return newObj
+}
 
+const levelUp = function (obj) {
+  const changeLevel = {
+    name: obj.name,
+    attackDamage: obj.attackDamage,
+    boss: obj.boss,
+    level: obj.level + 1,
+    alive: obj.alive,
+    hitPoints: obj.hitPoints
+  }
+return changeLevel
+}
 
+const buff = function (obj) {
+  const increaseDamage = {
+    name: obj.name,
+    attackDamage: obj.attackDamage + 15,
+    boss: obj.boss,
+    level: obj.level, 
+    alive: obj.alive,
+    hitPoints: obj.hitPoints
+  }
+return increaseDamage
+}
 
+const makeSuper = function (obj) {
+  const addSuper = {
+    name: "Super " + obj.name,
+    attackDamage: obj.attackDamage,
+    boss: obj.boss,
+    level: obj.level,
+    alive: obj.alive,
+    hitPoints: obj.hitPoints
+  }
+return addSuper
+}
 
+const hitEnemy = function (obj) {
+  const reduceAndDie = {
+    name: obj.name,
+    attackDamage: obj.attackDamage,
+    boss: obj.boss,
+    level: obj.level,
+    alive: obj.hitPoints - 10 > 0 ? true : false,
+    hitPoints: obj.hitPoints - 10,
+  } 
+  return reduceAndDie
+}
 
-
-
-
+const beastMode = function (obj) {
+  const makeItBeastly = {
+    name: "Super " + obj.name,
+    attackDamage: obj.attackDamage + 15,
+    boss: true,
+    level: obj.level + 1,
+    alive: obj.alive,
+    hitPoints: obj.hitPoints,
+  }
+return makeItBeastly
+}
 
 
 // Our code here. Don't touch!
